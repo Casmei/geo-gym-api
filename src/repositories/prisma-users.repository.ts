d0f841d8cf52dfa32672d759f2213e42fa1aa890
Tiago C. Lima <casmei@protonmail.com>
2025-07-15 23:01:55 -0300

    refact: improve code style

--- src/repositories/prisma-users.repository.ts
+++ src/repositories/prisma-users.repository.ts
@@ -1,1 +1,10 @@
-export class PrismaUsersRepository {}
+import { prisma } from "@/lib/prisma";
+import type { Prisma } from "generated/prisma";
+
+export class PrismaUsersRepository {
+  async create(data: Prisma.UserCreateInput) {
+    const user = await prisma.user.create({ data });
+
+    return user;
+  }
+}
--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,30 +1,30 @@
 import { prisma } from "@/lib/prisma";
 import { hash } from "bcryptjs";
 
+export class RegisterUseCase {
+  // biome-ignore lint/suspicious/noExplicitAny: Vai falar disso nas próximas aulas
+  constructor(private usersRepository: any) {}
+  async execute({ email, name, password }: RegisterUseCaseRequest) {
+    const userWithSameEamil = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (userWithSameEamil) {
+      throw new Error("Email already exists");
+    }
+
+    const passoword_hash = await hash(password, 6);
+
+    await this.usersRepository.create({
+      name,
+      email,
+      passoword_hash,
+    });
+  }
+}
+
 interface RegisterUseCaseRequest {
   name: string;
   email: string;
   password: string;
-}
-
-export async function registerUseCase({
-  email,
-  name,
-  password,
-}: RegisterUseCaseRequest) {
-  const userWithSameEamil = await prisma.user.findUnique({ where: { email } });
-
-  if (userWithSameEamil) {
-    throw new Error("Email already exists");
-  }
-
-  const passoword_hash = await hash(password, 6);
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      passoword_hash,
-    },
-  });
 }
