7b55512785bb4a8468359bfc69537066f0e3ed97
Tiago C. Lima <casmei@protonmail.com>
2025-07-15 23:11:32 -0300

    feat: add repository interface

---
+++ src/repositories/prisma/prisma-users.repository.ts
@@ -0,0 +1,16 @@
+import { prisma } from "@/lib/prisma";
+import type { Prisma } from "generated/prisma";
+import type { UsersRepository } from "../users-repository";
+
+export class PrismaUsersRepository implements UsersRepository {
+  async findByEmail(email: string) {
+    return await prisma.user.findUnique({
+      where: { email },
+    });
+  }
+  async create(data: Prisma.UserCreateInput) {
+    const user = await prisma.user.create({ data });
+
+    return user;
+  }
+}
