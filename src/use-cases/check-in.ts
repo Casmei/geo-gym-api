f6c67675217e8021aca3c3f6f1583432ad90e019
Tiago C. Lima <casmei@protonmail.com>
2025-07-16 09:42:46 -0300

    feat: add check-in use case

---
+++ src/use-cases/check-in.ts
@@ -0,0 +1,25 @@
+import type { CheckIn } from "generated/prisma";
+import type { CheckInsRepository } from "@/repositories/check-ins-repository";
+
+interface CheckInUseCaseRequest {
+  userId: string;
+  gymId: string;
+}
+
+type CheckInUseCaseResponse = { checkIn: CheckIn };
+
+export class CheckInUseCase {
+  constructor(private checkInsRespository: CheckInsRepository) {}
+
+  async execute({
+    gymId,
+    userId,
+  }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    const checkIn = await this.checkInsRespository.create({
+      gym_id: gymId,
+      user_id: userId,
+    });
+
+    return { checkIn };
+  }
+}
