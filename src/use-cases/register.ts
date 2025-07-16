40d7761cf49b8d2520b6e4e35379d8f25187c0c2
Tiago C. Lima <casmei@protonmail.com>
2025-07-15 23:56:29 -0300

    test: register  use case unit test

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -2,10 +2,25 @@ import { prisma } from "@/lib/prisma";
 import type { UsersRepository } from "@/repositories/users-repository";
 import { hash } from "bcryptjs";
 import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
+import type { User } from "generated/prisma";
+
+interface RegisterUseCaseRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+interface RegisterUseCaseResponse {
+  user: User;
+}
 
 export class RegisterUseCase {
   constructor(private usersRepository: UsersRepository) {}
-  async execute({ email, name, password }: RegisterUseCaseRequest) {
+  async execute({
+    email,
+    name,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
     const userWithSameEamil = await this.usersRepository.findByEmail(email);
 
     if (userWithSameEamil) {
@@ -14,16 +29,12 @@ export class RegisterUseCase {
 
     const passoword_hash = await hash(password, 6);
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       passoword_hash,
     });
-  }
-}
 
-interface RegisterUseCaseRequest {
-  name: string;
-  email: string;
-  password: string;
+    return { user };
+  }
 }
