05483eaa4c6ee6396d0872db1beef07dc7b47637
Tiago C. Lima <casmei@protonmail.com>
2025-07-19 01:02:01 -0300

    feat: config e2e testing

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,25 @@ import { coverageConfigDefaults, defineConfig } from "vitest/config";
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
+    dir: "src",
+    projects: [
+      {
+        extends: true,
+        test: {
+          name: "unit",
+          dir: "src/use-cases",
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: "e2e",
+          dir: "src/http/controllers",
+          environment:
+            "./prisma/vitest-enviroment-prisma/prisma-test-enviroment.ts",
+        },
+      },
+    ],
     coverage: {
       reporter: ["text", "lcov", "html"],
       include: ["src/use-cases/**/*.ts"],
